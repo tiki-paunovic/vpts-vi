73e29366871f913ff3f618efc4fed37c3258fd87
tiki-paunovic <tiki.paunovic@gmail.com>
2020-02-28 22:43:41 +0100

    samo test

--- fibonaci.py
+++ fibonaci.py
@@ -1,3 +1,4 @@
+
 n = int(input ("Koliko Fibonačijevih brojeva treba štampati: "))
 br1 = 1
 br2 =1
@@ -7,3 +8,7 @@ for i in range(3,n+1):
     br = br1+br2
     print (br, end = ' ')
     br1,br2 = br2, br
+input ()
+
+
+
