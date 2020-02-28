389b7b90cf7bd1fe873d6a9769cf53d9316d6666
tiki-paunovic <34627438+tiki-paunovic@users.noreply.github.com>
2020-02-28 22:27:17 +0100

    Create fibonaci.py

---
+++ fibonaci.py
@@ -0,0 +1,9 @@
+n = int(input ("Koliko Fibonačijevih brojeva treba štampati: "))
+br1 = 1
+br2 =1
+print (br1, br2, end=' ')
+
+for i in range(3,n+1):
+    br = br1+br2
+    print (br, end = ' ')
+    br1,br2 = br2, br
